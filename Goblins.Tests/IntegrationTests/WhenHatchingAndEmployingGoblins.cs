2972dee54d7eaea7a359b6bcbdac9b5b67f3c87e
Lawrence Anstey <lawrence.anstey@4mat.com>
2023-03-19 23:13:47 +0000

    injecting a test output helper so we can see what happens when the test runs

--- Goblins.Tests/IntegrationTests/WhenHatchingAndEmployingGoblins.cs
+++ Goblins.Tests/IntegrationTests/WhenHatchingAndEmployingGoblins.cs
@@ -1,6 +1,6 @@
 ﻿using FluentAssertions;
 using Goblins.Core;
-using Goblins.Core.Tools;
+using Xunit.Abstractions;
 
 namespace Goblins.Tests.IntegrationTests
 {
@@ -8,31 +8,36 @@ namespace Goblins.Tests.IntegrationTests
     {
         private IEnumerable<Goblin> employedGoblins;
 
-        public WhenHatchingAndEmployingGoblins() =>
+        static TestGoblinProvider provider = new();
+
+        public WhenHatchingAndEmployingGoblins(ITestOutputHelper outputHelper)
+        {
+            var hatchery = new GoblinHatchery(provider, outputHelper);
+            
+            var hatchedGoblins = hatchery.Hatch();
+
             employedGoblins = new GoblinEmploymentAgency()
-                .Employ(
-                    new GoblinHatchery(
-                        new TestGoblinProvider()
-                        ).Hatch());
+                .Employ(hatchedGoblins);
+        }
 
         [Fact]
-        public void ThenAllGoblinsGetJobs() =>
-            employedGoblins.Should().AllSatisfy(goblin => goblin.Job.Should().NotBeNullOrEmpty());
-        
+        public void ThenAllGoblinsGetJobs_RegularMode()
+        {
+            foreach (var goblin in employedGoblins)
+            {
+                goblin.Job.Should().NotBeNullOrEmpty();
+                goblin.Name.Should().NotBeNullOrEmpty();
+            }
+        }
+
         [Fact]
-        public void ThenAllRedPickaxeGoblinsAreMiners() =>
-            employedGoblins
-                .Where(goblin => goblin.Colour == Colour.Red)
-                .Where(goblin => goblin.Tools.OfType<Pickaxe>().Any())
-                .Should()
-                .AllSatisfy(goblin => goblin.Job.Should().Be("Miner"));
-        
-        [Fact]
-        public void ThenAllRedPenGoblinsAreWriters() =>
-            employedGoblins
-                .Where(goblin => goblin.Colour == Colour.Red)
-                .Where(goblin => goblin.Tools.OfType<Pen>().Any())
-                .Should()
-                .AllSatisfy(goblin => goblin.Job.Should().Be("Writer"));
+        public void ThenAllGoblinsGetJobs_FluentAllTheWayMode() =>
+            employedGoblins.Should()
+            .AllSatisfy(goblin =>
+            {
+                goblin.Job.Should().NotBeNullOrEmpty();
+                goblin.Name.Should().NotBeNullOrEmpty();
+
+            });
     }
 }
